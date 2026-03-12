09ba31fde9def2fd7f20eecfc20691e8ab1d4169
Rafal Dittwald <rafal.dittwald@gmail.com>
2026-03-11 22:00:03 -0400

    Move dat out into its own lib

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject quadvote "0.0.1"
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/clojurescript "1.12.134" :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [io.bloomventures/omni "0.36.0"]
+                 [io.bloomventures/omni "0.36.3"]
                  [io.bloomventures/commons "0.17.1"]
                  [com.github.rafd/sys "0.3.2"]
+                 [com.github.rafd/dat "0.0.1-20260306-0"]
 
                  ;; [com.hyperfiddle/rcf "20220926-202227"] from commons
 
@@ -16,7 +17,7 @@
                  [tada "0.3.1"]]
   :main quadvote.core
 
-  :plugins [[io.bloomventures/omni "0.36.0"]]
+  :plugins [[io.bloomventures/omni "0.36.3"]]
   :omni-config quadvote.omni-config/omni-config
 
   :profiles {:dev {:source-paths ["src" "dev-src"]
